ed3ac12680f398e3340246f7468ba1de46bfbcd6
iankuria668 <iankuria668@gmail.com>
2024-07-19 15:36:47 +0300

    icons installed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,41 @@
 import React from "react";
 import  "../index.css";
+import { AiFillHome } from "react-icons/ai";
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineBulb } from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
 function Navbar(){
+
     return(
         <div className="navbar-container">
+
             <div className="profile">
-            <img 
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPpd2dKUULlTHDgoufs2wFrK7KQlYsPp5WFw&s"
-            id="profile-picture"
-            alt="profile"
-            />
+                <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPpd2dKUULlTHDgoufs2wFrK7KQlYsPp5WFw&s"
+                id="profile-picture"
+                alt="profile"
+                />
             </div>
+
             <div>
-            <nav>
-                    <a href="/"><img src="https://cdn-icons-png.flaticon.com/128/3917/3917032.png" className="icon" alt="Home"/></a>
+                <nav>
+                    < a href="/"><AiFillHome /></a>
+                </nav>
+
+                <nav>
+                    <a href="/discover"><AiOutlineBulb /></a>
                 </nav>
                 <nav>
-                    <a href="/discover"><img src="https://cdn-icons-png.flaticon.com/128/5069/5069162.png" className="icon" alt="Discover"/></a>
-                </nav>
-                <nav>
-                    <a href="/search"><img src="https://cdn-icons-png.flaticon.com/128/3917/3917132.png" className="icon" alt="Search"/></a>
+                    <a href="/search"><AiOutlineSearch /></a>
                 </nav>
             </div> 
+
             <div>
-            <nav>
-                    <a href="/library"><img src="https://cdn-icons-png.flaticon.com/128/3917/3917215.png" className="icon" alt="Library"/></a>
+                <nav>
+                    <a href="/library"><AiOutlineMenu /></a>
                 </nav>
             </div> 
+
         </div>
     )
 }
