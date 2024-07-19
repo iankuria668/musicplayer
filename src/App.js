8d4990d7be0bfcf63ce4329264db409881cde339
iankuria668 <iankuria668@gmail.com>
2024-07-19 14:04:56 +0300

    Router configured

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './screens/Home';
-import NotFound from './screens/NotFound';
+import Discover from './screens/Discover';
+import Library from './screens/Library';
+import Search from './screens/Search';
+import Notfound from './screens/Notfound';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/discover" element={<Discover />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
