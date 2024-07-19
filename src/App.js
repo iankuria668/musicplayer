81dbc191257ff7f441e99c80dd933dfb68845ed3
iankuria668 <iankuria668@gmail.com>
2024-07-19 13:59:57 +0300

    Page functions created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './screens/Home';
+import NotFound from './screens/NotFound';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <p>Hello World</p>
     </div>
   );
 }
--- src/screens/Notfound.js
+++ src/screens/Notfound.js
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default function Notfound() {
+function Notfound() {
     return <div>Notfound</div>;
 }
+
+export default Notfound;
